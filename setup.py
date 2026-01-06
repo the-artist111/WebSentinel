a8952b2ab8533c1af89bf6b48b8f18b5103b93cb
The-Artist <fardinislamop@gmail.com>
2026-01-06 11:08:39 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
 
+from setuptools import setup
+
+setup(
+    name="websentinel",
+    version="1.0.0",
+    description="Advanced web application vulnerability scanner for SQLi, XSS, and CSRF",
+    author="the-artist111",
+    license="MIT",
+
+    py_modules=[
+        "websentinel",
+        "crawler",
+        "scanner",
+        "report"
+    ],
+
+    install_requires=[
+        "requests",
+        "beautifulsoup4"
+    ],
+
+    python_requires=">=3.8",
+
+    entry_points={
+        "console_scripts": [
+            "websentinel=websentinel:main"
+        ]
+    },
+)
